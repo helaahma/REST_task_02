75f18d4eb644e371f8c7a6f10b1a6d94e927f3f9
Ahmad Helal <you@example.com>
2019-07-23 20:03:36 +0300

    Aziz and Ahmad Helal

--- flights/views.py
+++ flights/views.py
@@ -1,8 +1,8 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from datetime import datetime
 
 from .models import Flight, Booking
-from .serializers import FlightSerializer, BookingSerializer
+from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, BookingUpdateSerializer
 
 
 class FlightsList(ListAPIView):
@@ -13,5 +13,29 @@ class FlightsList(ListAPIView):
 class BookingsList(ListAPIView):
 	queryset = Booking.objects.filter(date__gte=datetime.today())
 	serializer_class = BookingSerializer
+
+class BookingDetails(RetrieveAPIView):
+	queryset = Booking.objects.all()
+	serializer_class = BookingDetailsSerializer
+	lookup_field= "id"
+	lookup_url_kwarg="obj"
+
+class BookingUpdate(RetrieveUpdateAPIView):
+	queryset = Booking.objects.all()
+	serializer_class = BookingUpdateSerializer
+	lookup_field= "id"
+	lookup_url_kwarg="obj"
+
+
+class BookingDelete(DestroyAPIView):
+	queryset = Booking.objects.all()
+	lookup_field= "id"
+	lookup_url_kwarg="obj"
+
+
+
+
+
+
 
 
--- task_1/urls.py
+++ task_1/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('flights/', views.FlightsList.as_view(), name="flights-list"),
     path('bookings/', views.BookingsList.as_view(), name="bookings-list"), 
+    path('detail/<int:obj>/', views.BookingDetails.as_view(), name="booking-details"),
+    path('update/<int:obj>/', views.BookingUpdate.as_view(), name="update-booking"),
+    path('delete/<int:obj>/', views.BookingDelete.as_view(), name="cancel-booking")
 ]
